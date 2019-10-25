aa4f197df7b01bee39cfd5273a8aa0343033d5b1
donot <donot@donot.me>
2019-10-25 21:06:11 +0800

    完善api接口

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 	path('<apikey>/weblog/<hashstr>', views.weblog),
 	path('<apikey>/dnslog/<hashstr>', views.dnslog),
+	path('<apikey>/clear/all', views.clear),
 	re_path('.*', views.notfound),
 ]
--- api/views.py
+++ api/views.py
@@ -8,19 +8,16 @@ from logview.models import DnsLog,WebLog,UserSubDomain
 def weblog(request, apikey, hashstr):
 	user = ApiKey.objects.filter(key=apikey)
 	if user:
-		username = user[0].user.username
-		# subdomain = UserSubDomain.objects.filter(user=user[0].user)[0].subdomain
 		weblogs = WebLog.objects.filter(user=user[0].user).filter(path__contains=hashstr)
 		data = []
 		ret = {}
 		if weblogs:
-			for weblog in weblogs:
-				data.append({'path': weblog.path, 'ip': weblog.ip, 'header': weblog.header, 'body': weblog.body})
+			for weblogi in weblogs:
+				data.append({'path': weblogi.path, 'ip': weblogi.ip, 'header': weblogi.header, 'body': weblogi.body})
 			ret = {'status': 1, 'msg': '', 'total': len(weblogs), 'data': data}
 		else:
 			ret = {'status': 1, 'msg': '', 'total': 0, 'data': []}
 	else:
-		username = ''
 		ret = {'status': -1, 'msg': 'api key not found!'}
 	return JsonResponse(ret)
 
@@ -29,10 +26,30 @@ def weblog(request, apikey, hashstr):
 def dnslog(request, apikey, hashstr):
 	user = ApiKey.objects.filter(key=apikey)
 	if user:
-		username = user[0].user.username
+		dnslogs = DnsLog.objects.filter(user=user[0].user).filter(host__contains=hashstr)
+		data = []
+		if dnslogs:
+			for dnslogi in dnslogs:
+				data.append({'host': dnslogi.host, 'log_time': dnslogi.log_time})
+			ret = {'status': 1, 'msg': '', 'total': len(data), 'data': data}
+		else:
+			ret = {'status': 1, 'msg': '', 'total': 0, 'data': data}
 	else:
-		username = ''
-	return HttpResponse('OK')
+		ret = {'status': -1, 'msg': 'api key not found!'}
+	return JsonResponse(ret)
+
+
+
+def clear(request, apikey):
+	user = ApiKey.objects.filter(key=apikey)
+	if user:
+		DnsLog.objects.filter(user=user[0].user).delete()
+		WebLog.objects.filter(user=user[0].user).delete()
+		ret = {'status': 1, 'msg': ''}
+	else:
+		ret = {'status': -1, 'msg': 'api key not found!'}
+	return JsonResponse(ret)
+
 
 
 
